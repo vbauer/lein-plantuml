172979c1123e056d7a072939b8e88defee6edbf6
Vladislav Bauer <bauer.vlad@gmail.com>
2014-05-26 13:07:00 +0700

    add lein-ancient plugin, update plantuml 7997->7999

--- project.clj
+++ project.clj
@@ -4,12 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[net.sourceforge.plantuml/plantuml "7997"]
+  :dependencies [[net.sourceforge.plantuml/plantuml "7999"]
                  [clj-glob "1.0.0" :exclusions [org.clojure/clojure]]]
 
   :plugins [[lein-release "1.0.5" :exclusions [org.clojure/clojure]]
             [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
-            [lein-bikeshed "0.1.7" :exclusions [org.clojure/clojure]]]
+            [lein-bikeshed "0.1.7" :exclusions [org.clojure/clojure]]
+            [lein-ancient "0.5.5"]]
 
   :eval-in-leiningen true
   :pedantic? :abort

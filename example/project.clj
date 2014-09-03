c9a01a20c6024a2107b13e00426aab6fb7d1059e
Vladislav Bauer <bauer.vlad@gmail.com>
2014-09-03 21:08:22 +0700

    Update version (0.1.8->0.1.9) of plugin in the example project

--- example/project.clj
+++ example/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :plugins [[lein-plantuml "0.1.8"]]
+  :plugins [[lein-plantuml "0.1.9"]]
   :plantuml [["doc/*.puml" :png "target/doc"]])

29a2cc988d931f6f29b890912f990740a78d2614
Vladislav Bauer <bauer.vlad@gmail.com>
2014-10-30 14:24:57 +0600

    Update .gitignore, downgrade lein-release plugin

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [clj-glob "1.0.0" :exclusions [org.clojure/clojure]]]
 
   :plugins [[jonase/eastwood "0.1.4" :exclusions [org.clojure/clojure]]
-            [lein-release "1.1.3" :exclusions [org.clojure/clojure]]
+            [lein-release "1.0.6" :exclusions [org.clojure/clojure]]
             [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
             [lein-bikeshed "0.1.8" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.5.5"]]

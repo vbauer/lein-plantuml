c94f0ded6b984fae1550a2ee6085fc07e301ab6d
Vladislav Bauer <bauer.vlad@gmail.com>
2014-10-30 14:18:44 +0600

    Update lein-bikeshed & lein-release plugin

--- project.clj
+++ project.clj
@@ -9,9 +9,9 @@
                  [clj-glob "1.0.0" :exclusions [org.clojure/clojure]]]
 
   :plugins [[jonase/eastwood "0.1.4" :exclusions [org.clojure/clojure]]
-            [lein-release "1.0.5" :exclusions [org.clojure/clojure]]
+            [lein-release "1.1.3" :exclusions [org.clojure/clojure]]
             [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
-            [lein-bikeshed "0.1.7" :exclusions [org.clojure/clojure]]
+            [lein-bikeshed "0.1.8" :exclusions [org.clojure/clojure]]
             [lein-ancient "0.5.5"]]
 
   :eval-in-leiningen true

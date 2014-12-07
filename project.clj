cad3b61a5d534177e1eb0dce145902f8bab5cbf7
Vladislav Bauer <bauer.vlad@gmail.com>
2014-12-07 19:27:23 +0600

    lein-release plugin: preparing 0.1.12 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-plantuml "0.1.12-SNAPSHOT"
+(defproject lein-plantuml "0.1.12"
   :description "A Leiningen plugin for generating UML diagrams using PluntUML."
   :url "https://github.com/vbauer/lein-plantuml"
   :license {:name "Eclipse Public License"

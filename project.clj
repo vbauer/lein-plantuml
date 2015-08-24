961e5cae85a40a62bd6f37be67b12a200d6d238b
Vladislav Bauer <bauer.vlad@gmail.com>
2015-08-25 02:37:38 +0500

    Update plantuml

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.apache.xmlgraphics/batik-rasterizer "1.8"]
-                 [net.sourceforge.plantuml/plantuml "8026"]
+                 [net.sourceforge.plantuml/plantuml "8029"]
                  [me.raynes/fs "1.4.6" :exclusions [org.clojure/clojure]]]
 
   :profiles {

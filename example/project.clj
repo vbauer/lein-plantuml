13b559353ed60cda1907bceab8782e5fb1fd424e
Vladislav Bauer <vbauer@yandex-team.ru>
2018-11-11 19:49:59 +0700

    Update plugin version to 0.1.22 in example project

--- example/project.clj
+++ example/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   ; List of plugins
-  :plugins [[lein-plantuml "0.1.20-SNAPSHOT"]]
+  :plugins [[lein-plantuml "0.1.22"]]
 
   ; List of hooks
   ; It's used for running lein-plantuml during compile phase
